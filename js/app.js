7473d515198edea80861131c6c51303f71689f84
Jan Podgorski <j.pod.12@gmail.com>
2018-04-18 18:54:16 +0200

    added timer & win condition

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // list that holds all cards classes
-let cards = ['tiger','elephant','pig','sheep','cow','cat','dog','zebra','tiger','elephant','pig','sheep','cow','cat','dog','zebra'];
+let cards = ['tiger', 'elephant', 'pig', 'sheep', 'cow', 'cat', 'dog', 'zebra', 'tiger', 'elephant', 'pig', 'sheep', 'cow', 'cat', 'dog', 'zebra'];
 let openCards = [];
 const deck = document.querySelector(".deck"); //selecting deck list
 const fragment = document.createDocumentFragment(); // DocumentFragment wrapper
@@ -9,6 +9,13 @@ const secondStar = document.querySelector(".second-star"); //secondStar
 const thirdStar = document.querySelector(".third-star"); //thirdStar
 let moves = 0; //moves counter
 let pairs = 0; //matched pairs counter
+
+let time = '00:00'
+let seconds = 0;
+let minutes = 0;
+let timeTiger = 0;
+let t;
+const timer = document.querySelector('.timer');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -27,21 +34,21 @@ function shuffle(array) {
 
 //Creating card list
 function makeList() {
-    deck.innerHTML ='';//cleaning card list
-    for(let card of shuffle(cards)) {
+    deck.innerHTML = '';//cleaning card list
+    for (let card of shuffle(cards)) {
 
         let cardWrap = document.createElement('li');
         cardWrap.classList.add("card-wrap");
 
         let front = document.createElement('span');
-        front.classList.add("front","card");
+        front.classList.add("front", "card");
         cardWrap.appendChild(front);
 
         let back = document.createElement('span');
-        back.classList.add("back","card");
+        back.classList.add("back", "card");
 
         let content = document.createElement('i');
-        content.classList.add("svg",`${card}`);
+        content.classList.add("svg", `${card}`);
         back.appendChild(content);
 
         cardWrap.appendChild(back);
@@ -65,23 +72,46 @@ function makeList() {
 
 //Reset after button click
 const restart = document.querySelector(".restart");
-restart.addEventListener('click',function(e){
+restart.addEventListener('click', function (e) {
     e.preventDefault();
     makeList();
+    resetMoves();
+    resetStars();
+    resetTimer();
+});
+
+//reset moves
+function resetMoves() {
     moves = 0;
     moveCounter.textContent = moves;
+}
+
+//reset stars
+function resetStars() {
     firstStar.className = 'first-star fa fa-star';
     secondStar.className = 'second-star fa fa-star';
     thirdStar.className = 'third-star fa fa-star';
-});
+}
+
+//reset timer
+function resetTimer() {
+    clearInterval(t);
+    seconds = 0;
+    minutes = 0;
+    timer.textContent = time;
+}
 
 //listen for click and flip card
 function turn() {
-    deck.addEventListener('click', function(e){
+    deck.addEventListener('click', function (e) {
         e.preventDefault();
-        if(e.target.nodeName === 'SPAN'){
-                cardList(e.target);
-                match(e.target);
+        timeTiger++
+        if (timeTiger === 1) {
+            startTimer();
+        }
+        if (e.target.nodeName === 'SPAN') {
+            cardList(e.target);
+            match(e.target);
         }
     });
 }
@@ -92,39 +122,67 @@ function cardList(target) {
 }
 
 //flipping cards function
-function flip(target){
+function flip(target) {
     target.classList.add('open-front');
     target.nextElementSibling.classList.add('open-back', 'animated');
 }
 
 //Star rating function for removing stars when moves counter is increasing
-function starRating (m){
-    if (m >= 10 && m < 12 ){
+function starRating(m) {
+    if (m >= 10 && m < 12) {
         firstStar.classList.remove('fa-star');
         firstStar.classList.add('fa-star-half-o');
-    } else if (m >= 12 && m < 14){
+    } else if (m >= 12 && m < 14) {
         firstStar.classList.remove('fa-star-half-o');
         firstStar.classList.add('fa-star-o');
-    } else if(m >= 14 && m < 16){
+    } else if (m >= 14 && m < 16) {
         secondStar.classList.remove('fa-star');
         secondStar.classList.add('fa-star-half-o');
-    } else if(m >= 16 && m < 18){
+    } else if (m >= 16 && m < 18) {
         secondStar.classList.remove('fa-star-half-o');
         secondStar.classList.add('fa-star-o');
-    } else if(m >= 18 && m < 20){
+    } else if (m >= 18 && m < 20) {
         thirdStar.classList.remove('fa-star');
         thirdStar.classList.add('fa-star-half-o');
-    } else if(m >= 20){
+    } else if (m >= 20) {
         thirdStar.classList.remove('fa-star-half-o');
         thirdStar.classList.add('fa-star-o');
     }
+}
 
+//timer function
+function startTimer() {
+    clearInterval(t);
+    t = setInterval(buildTimer, 1000);
+}
+
+timer.textContent = time;
+
+function buildTimer() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            seconds = 0;
+        }
+    }
+    timer.textContent = (minutes < 10 ? "0" + minutes.toString() : minutes) + ":" + (seconds < 10 ? "0" + seconds.toString() : seconds);
+}
+
+function stopGame() {
+    if (pairs === 8) {
+        console.log("YOU WON")
+        clearInterval(t);
+
+    }
 }
 
 //checking if cards match
 function match(target) {
     flip(target);
-    if (openCards.length === 2){
+    if (openCards.length === 2) {
         moves += 1; //adding move
         moveCounter.textContent = moves;
         starRating(moves);
@@ -133,22 +191,24 @@ function match(target) {
         //adding match class to cards that are the same
         if (firstCard.classList[1] === secondCard.classList[1]) {
             pairs += 1; //adding matched pairs
-            setTimeout(function(){
+            setTimeout(function () {
                 firstCard.parentNode.classList.add('match', 'pulse');
                 secondCard.parentNode.classList.add('match', 'pulse');
             }, 400);
-        //turning cards back
-        } else if (firstCard.classList[1] !== secondCard.classList[1]){
-            setTimeout(function(){
+            //turning cards back
+        } else if (firstCard.classList[1] !== secondCard.classList[1]) {
+            setTimeout(function () {
                 firstCard.parentNode.classList.remove('open-back');
                 firstCard.parentNode.previousSibling.classList.remove('open-front');
                 secondCard.parentNode.classList.remove('open-back');
                 secondCard.parentNode.previousSibling.classList.remove('open-front');
             }, 1000);
         }
-       openCards = [];
+        openCards = [];
+        stopGame();
     }
 }
+
 makeList();
 turn();
 
